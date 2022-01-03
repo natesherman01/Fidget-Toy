c42487372165fc67b18d3b96bab94463e643783d
natesherman01 <96322028+natesherman01@users.noreply.github.com>
2022-01-02 17:00:32 -0800

    Update clk_div_2hz.sv

--- clk_div_2hz.sv
+++ clk_div_2hz.sv
@@ -16,24 +16,3 @@ module clk_div_2hz #(parameter COUNT = 23999999)(
     end
         
 endmodule
-
-Four Hz Clock Divider: clk_div_4hz.sv
-
-module clk_div_4hz #(parameter COUNT = 11999999)(
-    input CLK,
-    output logic SCLK = 0
-    );
-    
-    logic [31:0] count = 0;
-    always_ff @ (posedge CLK)
-    begin
-        if (count == COUNT -1 )
-        begin
-            SCLK <= ~SCLK;
-            count <= 0;
-        end
-        else
-            count <= count + 1;
-    end
-        
-endmodule
